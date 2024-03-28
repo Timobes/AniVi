6e7ae9da48ced3c1c693479efa9a45931198634c
Tim <Obertim@yandex.ru>
2024-03-28 19:05:47 +0300

    add comment 3

--- backend/router/usersRouter.js
+++ backend/router/usersRouter.js
@@ -6,7 +6,6 @@ const userController = require('../controller/userController')
 // userRouter.get('/', userController.getUsers)
 userRouter.get('/', userController.getUsers)
 
-// userRouter.get('/:id', userController.getUser)
 userRouter.get('/:id', userController.getUser)
 
 module.exports = userRouter
